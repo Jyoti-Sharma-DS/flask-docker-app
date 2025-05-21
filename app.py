d57c19ad8414f7c684dd5c0caf49cd9bc84faf55
jsJyo <48561177+jsJyo@users.noreply.github.com>
2025-05-21 13:04:41 -0400

    Add Flask Docker app with .gitignore

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import FLASK
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -8,4 +8,4 @@ def hello():
     return "Hello from Flask in Docker!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5001)
